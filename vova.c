4bd0d2020c5ca9c0c254725f6a74bc941b1a9c75
temirovavika <vika.temirova.06@inbox.ru>
2024-09-09 23:58:23 +0300

    Add files via upload

---
+++ vova.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void sortirovka_of_numbers(int* numbers, int stolbec);
+int main(void)
+{
+    int numbers[] = {8, 9, 4, 2, 7, 3, 6, 1};
+    int stolbec = sizeof(numbers) / sizeof(numbers[0]);
+    sortirovka_of_numbers(numbers, stolbec);
+    for (int g = 0; g < stolbec; g++)
+    {
+        printf(" %d ", numbers[g]);
+    }
+
+}
+void sortirovka_of_numbers(int* numbers, int stolbec)
+{
+    for (int i = 0; i < stolbec - 1 ; i++ )
+    {
+        for (int j = 0; j < stolbec - i - 1; j++)
+        {
+            if (numbers[j] > numbers[j+1])
+            {
+                int temp = numbers[j];
+                           numbers[j] = numbers[j+1];
+                                        numbers[j+1] = temp;
+            }
+        }
+    }
+
+    /*for (int g = 0; g < stolbec; g++)
+    {
+        printf(" %d ", numbers[g]);
+    } */
+
+
+}
